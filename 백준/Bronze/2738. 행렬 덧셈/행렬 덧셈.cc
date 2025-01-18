89379bb1dd433d4014813ea81399c374106e3e76
김태현 <143389711+taehyun00@users.noreply.github.com>
2025-01-19 05:23:55 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -1,32 +1,28 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 
 int main() {
-    int n, m;
-    scanf("%d %d", &n, &m);
-    
-    int a[n][m], b[n][m]; 
-    
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            scanf("%d", &a[i][j]);
+    int n,m;
+    scanf("%d %d",&n,&m);
+    int a[n][m],b[n][m];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            scanf("%d",&a[i][j]);
         }
     }
     
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            scanf("%d", &b[i][j]);
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            scanf("%d",&b[i][j]);
         }
     }
     
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            printf("%d ", a[i][j] + b[i][j]);
+    for(int j=0; j<n; j++){
+        for(int k=0; k<m; k++){
+            printf("%d ",a[j][k]+b[j][k]);
         }
         printf("\n");
     }
-    
     return 0;
 }
+
