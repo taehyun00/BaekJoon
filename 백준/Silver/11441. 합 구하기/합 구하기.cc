eaaedd39d450b7d02afe0e1fbba8903f6e64faab
김태현 <143389711+taehyun00@users.noreply.github.com>
2025-02-08 11:12:02 +0900

    [Silver III] Title: 합 구하기, Time: 52 ms, Memory: 1380 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.cc
@@ -0,0 +1,22 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int num,t;
+    scanf("%d",&num);
+    int arr[num];
+    int start,end,sum=0;
+    for(int i=0; i<num; i++){
+        int temp;
+        scanf("%d",&temp);
+        arr[i] = arr[i-1] + temp;
+    }
+    scanf("%d",&t);
+    for(int i = 0; i<t; i++){
+        scanf("%d %d",&start,&end);
+        printf("%d\n",arr[end-1] - arr[start-2]);
+    }
+    
+
+    return 0;
+}
