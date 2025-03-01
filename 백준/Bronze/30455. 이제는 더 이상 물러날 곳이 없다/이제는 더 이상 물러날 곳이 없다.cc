6f10384cf5517ae7497d997ee3cf52737e5571e3
김태현 <143389711+taehyun00@users.noreply.github.com>
2025-03-01 11:16:15 +0900

    [Bronze I] Title: 이제는 더 이상 물러날 곳이 없다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/30455. 이제는 더 이상 물러날 곳이 없다/이제는 더 이상 물러날 곳이 없다.cc
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf("%d",&num);
+    if(num%2 == 1 ){
+        printf("Goose");
+        return 0;
+    } 
+    else printf("Duck");
+
+    return 0;
+}
