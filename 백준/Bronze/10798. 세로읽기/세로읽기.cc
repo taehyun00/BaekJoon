77e837bbde7b1ab6fb7b8df6e671909813e0d270
김태현 <143389711+taehyun00@users.noreply.github.com>
2025-03-06 08:18:21 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char arr[5][15] ={};
+    for(int i = 0 ; i<5; i++){
+            scanf("%s",&arr[i]);
+    }
+    
+    for(int i = 0 ; i<15; i++){
+        for(int j=0; j<5; j++){
+            if(arr[j][i]!='\0')printf("%c",arr[j][i]);
+        }
+
+    }
+
+    return 0;
+}
