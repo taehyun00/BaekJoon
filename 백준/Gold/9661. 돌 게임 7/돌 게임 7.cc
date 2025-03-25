9db41cfc28be471a8b9e83239afbceac294e06ed
김태현 <143389711+taehyun00@users.noreply.github.com>
2025-03-25 23:56:54 +0900

    [Gold II] Title: 돌 게임 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Gold/9661. 돌 게임 7/돌 게임 7.cc
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    long long int num;
+    scanf("%lld",&num);
+    if (num%5==0 || num%5==2){
+    printf("CY");
+    return 0;
+    }
+    else printf("SK");
+
+    return 0;
+}
+
+
