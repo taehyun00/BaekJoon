75c0d0bb034c9308078566f50e67e917c40fb04e
김태현 <143389711+taehyun00@users.noreply.github.com>
2025-12-16 10:50:51 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.cc
@@ -0,0 +1,9 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    (a>b) ? printf("flight") : printf("high speed rail");
+    return 0;
+}
