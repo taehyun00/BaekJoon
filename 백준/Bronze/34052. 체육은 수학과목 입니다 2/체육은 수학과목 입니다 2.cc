c63a9e3d276a388ea491ff0dfb70c38cac90721b
김태현 <143389711+taehyun00@users.noreply.github.com>
2025-12-20 06:50:30 +0900

    [Bronze V] Title: 체육은 수학과목 입니다 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/34052. 체육은 수학과목 입니다 2/체육은 수학과목 입니다 2.cc
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int a,b,c,d;
+   scanf("%d %d %d %d",&a,&b,&c,&d);
+   
+   (a+b+c+d <= 1500) ? printf("Yes") : printf("No");
+
+    return 0;
+}
