fca0f95b105b9274cfacd0456a2fb54097026861
x0gus <143389711+taehyun00@users.noreply.github.com>
2025-12-23 09:13:01 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.cc
@@ -0,0 +1,43 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    int num;
+    scanf ("%d",&num);
+    
+    switch(num){
+        case 1 :
+            printf("11\nA B C D E F G H J L M");
+            return 0;
+        case 2 :
+            printf("9\nA C E F G H I L M");
+            return 0;
+        case 3 :
+            printf("9\nA C E F G H I L M");
+            return 0;
+        case 4 :
+            printf("9\nA B C E F G H L M");
+            return 0;
+        case 5 :
+            printf("8\nA C E F G H L M");
+            return 0;
+        case 6 :
+            printf("8\nA C E F G H L M");
+            return 0;
+        case 7 :
+            printf("8\nA C E F G H L M");
+            return 0;
+        case 8 :
+            printf("8\nA C E F G H L M");
+            return 0;
+        case 9 :
+            printf("8\nA C E F G H L M");
+            return 0;
+        case 10 :
+            printf("8\nA B C F G H L M");
+            return 0;
+    }
+
+    return 0;
+}
