5eddc88eea0bbafee0b91022d53cd983a21f16c8
x0gux <143389711+x0gux@users.noreply.github.com>
2026-01-08 07:49:05 +0900

    [Bronze III] Title: 페르시아의 왕들, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10599. 페르시아의 왕들/페르시아의 왕들.cc
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a,b,c,d;
+    while(1){
+        scanf("%d %d %d %d",&a,&b,&c,&d);
+        
+        if(a == 0 && b== 0 && c == 0 && d == 0){
+            return 0 ;
+        }
+        
+        else{
+            printf("%d %d\n",c-b,d-a);
+        }
+    }
+
+    return 0;
+}
