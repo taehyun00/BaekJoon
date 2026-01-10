1e8285fad9c919403a75cd23af866fd06e6976c5
x0gux <143389711+x0gux@users.noreply.github.com>
2026-01-10 22:31:48 +0900

    [Bronze V] Title: Christmas Tree Adapter, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/32314. Christmas Tree Adapter/Christmas Tree Adapter.cc
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int k,m,n;
+    scanf("%d %d %d",&k,&m,&n);
+    
+    printf(m/n >= k ? "1" : "0");
+
+    return 0;
+}
