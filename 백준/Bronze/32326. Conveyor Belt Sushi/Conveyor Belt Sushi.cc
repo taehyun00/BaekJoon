4a0545223dce43eb1f9859caf67bbdf10e35f6e2
x0gux <143389711+x0gux@users.noreply.github.com>
2026-01-11 16:02:37 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.cc
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int r,g,b;
+    scanf("%d %d %d",&r,&g,&b);
+    printf("%d",r*3 + g*4 + b*5);
+
+    return 0;
+}
