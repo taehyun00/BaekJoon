1b72f2e21e65ff87701c1a253eb4b6b814d36764
x0gux <143389711+x0gux@users.noreply.github.com>
2026-01-12 16:48:42 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf("%d",&num);
+    
+    for(int i = 0 ; i<num; i++){
+        printf("LoveisKoreaUniversity ");
+    }
+    return 0;
+}
