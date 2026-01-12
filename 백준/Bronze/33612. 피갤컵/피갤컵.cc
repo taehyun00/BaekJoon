89fa7292e5b7e4e8b001620e414c26700bd13167
x0gux <143389711+x0gux@users.noreply.github.com>
2026-01-12 16:47:39 +0900

    [Bronze V] Title: 피갤컵, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/33612. 피갤컵/피갤컵.cc
@@ -0,0 +1,26 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf("%d",&num);
+    
+    switch(num){
+        case 1 :
+            printf("2024 8");
+            return 0;
+        case 2 :
+            printf("2025 3");
+            return 0;
+        case 3 :
+            printf("2025 10");
+            return 0;
+        case 4 :
+            printf("2026 5");
+            return 0;
+        case 5 :
+            printf("2026 12");
+            return 0;
+    }
+    return 0;
+}
