be568d4e23a7fd9952b2dfc7167120e594c46617
x0gux <143389711+x0gux@users.noreply.github.com>
2026-01-27 21:23:57 +0900

    [Bronze V] Title: 13 Floors, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/34412. 13 Floors/13 Floors.cc
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf("%d",&num);
+    if(num < 13 ) printf("%d",num);
+    else printf("%d",num+1);
+
+    return 0;
+}
